43a18bf44e2d7632bc7dcd8838d46c101ea1d6db
nkitsan <nastik.kitsan@gmail.com>
2018-11-08 13:15:22 +0300

    Fixed: validation of date

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
-const Joi = require('joi');
+const JoiBase = require('joi');
+const JoiExtension = require('joi-date-extensions');
+const Joi = JoiBase.extend(JoiExtension);
+
 
 const userSchema = {
     firstName: Joi.string().regex(/^(\w+)$/, 'name').required(),
@@ -8,7 +11,8 @@ const userSchema = {
     phone: Joi.string()
               .regex(/^[\+]?(375|80)[\s]?(29|25|44|33)[\s]?(\d{3})[\s\-]?(\d{2})[\s\-]?(\d{2})$/, 'phoneNumber')
               .required(),
-    dateOfRegistration: Joi.date().required()
+    dateOfRegistration: Joi.date().format(['YYYY-MM-DD', 'YYYY/MM/DD', 'YYYY.MM.DD', 'DD-MM-YYYY', 'DD.MM.YYYY', 'DD/MM/YYYY'])
+                           .required()
 };
 
 module.exports = userSchema;
