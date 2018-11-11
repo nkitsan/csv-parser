73d1ffddd87273a63a35da94455dfa58723fdb9b
nkitsan <nastik.kitsan@gmail.com>
2018-11-11 14:53:47 +0300

    Fixed: issue with async functions

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
 const csvParser = require('./libs/parseCsv');
+
 const Koa = require('koa');
-const Router = require('koa-router')
+const Router = require('koa-router');
 const busboy = require('koa-busboy');
-const send = require('koa-send');
+
+const fs = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
 
 
 const app = new Koa();
 const router = new Router();
 const uploader = busboy({
     dest: './input',
-    fnDestFilename: (fieldname, filename) => Date.now().toString() + filename
+    fnDestFilename: (fieldname, filename) => Date.now().toString() + '.csv'
 });
 
 router.post('/', uploader, async ctx => {
     const fileName = ctx.request.files[0].path;
-    const outputFile = csvParser.parseCsv(fileName, './output/');
-    //await send(ctx, outputFile);
+    const outputFile = await csvParser.parseCsv(fileName, './output/');
+    ctx.response.set('content-type', 'csv');
+    ctx.response.body = await readFileAsync(outputFile, {
+        encoding: 'utf8'
+    });
 });
 
 app.use(router.routes())
--- src/libs/parseCsv.js
+++ src/libs/parseCsv.js
@@ -1,59 +1,87 @@
 const Papa = require('papaparse');
 const Joi = require('joi');
+
 const fs = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
 const mongoClient = require("mongodb").MongoClient;
 const userSchema = require('./config');
 
-module.exports.parseCsv = function(fileName, outputDirectory) {
-    let data = '';
-    let users = [];
-    let readStream = fs.createReadStream(fileName, 'utf8');
 
-    readStream.on('data', function(chunk) {
-        data += chunk;
-    }).on('end', function() {
-        users = Papa.parse(data,{
-            header: true,
-            newline: "\r\n"
-        });
-        return validateUsers(users, outputDirectory);
+exports.parseCsv = async (fileName, outputDirectory) => {
+    const data = await readFileAsync(fileName, {
+        encoding: 'utf8'
     });
+
+    const users = Papa.parse(data, {
+        header: true,
+        newline: "\r\n"
+    });
+
+    const filePath = outputDirectory+Date.now().toString()+'.csv';
+
+    const isValid = await isParsingValid(users, filePath);
+    if (!isValid){
+        return filePath;
+    }
+
+    let validUsers = findValidUsers(users);
+    let invalidUsers = findInvalidUsers(users);
+
+    await writeParsingResults(validUsers, invalidUsers, filePath);
+    return filePath;
 };
 
-function validateUsers(users, outputDirectory){
-    const filename = outputDirectory+Date.now().toString()+'.csv';
-    let writeStream = fs.createWriteStream(filename);
-    if (users.errors.length){
-        writeStream.write(Papa.unparse(users.errors));
-        return filename;
-    }
+function findValidUsers(users){
     let correctUsers = [];
-    let invalidUsersInfo = [];
     for (let user of users.data){
         const validationResult = Joi.validate(user, userSchema);
-        if (validationResult.error){
-            invalidUsersInfo.push(Object.assign({},
-                                                {message: validationResult.error.details[0]['message']},
-                                                validationResult.error["_object"]));
-        }
-        else{
+        if (validationResult.error === null){
             correctUsers.push(validationResult.value);
         }
     }
-    if (correctUsers.length){
-        writeToDb(correctUsers);
-    }
-    if (invalidUsersInfo.length){
-        writeStream.write(Papa.unparse(invalidUsersInfo));
-    }
-    if (!invalidUsersInfo.length){
-        const response = [['status', 'message'],['200 OK', 'Successful validation']];
-        writeStream.write(Papa.unparse(response));
-    }
-    return filename;
+    return correctUsers;
 }
 
-function writeToDb(users) {
+function findInvalidUsers(users) {
+    let invalidUsers = [];
+    for (let user of users.data){
+        const validationResult = Joi.validate(user, userSchema);
+        if (validationResult.error){
+            invalidUsers.push(Object.assign({},
+                {message: validationResult.error.details[0]['message']},
+                validationResult.error["_object"]));
+        }
+    }
+    return invalidUsers;
+}
+
+async function isParsingValid(users, filePath){
+    if (users.errors.length){
+        await writeFileAsync(filePath, users);
+        return false;
+    }
+    return true;
+}
+
+
+async function writeParsingResults(validUsers, invalidUsers, filePath) {
+    if (validUsers.length){
+        correctUsersToDb(validUsers);
+    }
+    if (invalidUsers.length){
+        await writeFileAsync(filePath, Papa.unparse(invalidUsers));
+    }
+    if (!invalidUsers.length){
+        const response = [['status', 'message'],['200 OK', 'Successful validation']];
+        await writeFileAsync(filePath, Papa.unparse(response));
+    }
+}
+
+
+function correctUsersToDb(users) {
     const url = "mongodb://localhost:27017/";
     mongoClient.connect(url, (err, client)=>{
         const db = client.db("usersdb");
